dd07154639cc79caaea065485735a29df568fa77
manasesmburu20 <manasesmburu20@gmail.com>
2025-01-13 17:11:07 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'home_page.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(TravelExplorerApp());
 }
 
